c8d25ac58bc02848a6024f1953ae231c4c81b955
Loki88 <laplacetransformed@gmail.com>
2014-07-03 16:43:40 +0200

    Solved quadratic sieve squares generation bug

--- models/FactorizationMethod.py
+++ models/FactorizationMethod.py
@@ -22,6 +22,7 @@ __copyright__   = "Copyright 2014"
 from utility.Math import gcd, legendre
 from models import SettingsSingleton
 from random import randint
+from cmath import phase
 import numpy as np
 from decimal import *
 from math import sqrt
@@ -31,8 +32,15 @@ from utility.Math import continued_fraction_next_step, continued_fraction, smart
 import time
 
 
-
-n_primes = []
+mtest = MillerRabinTest()
+n_primes = [2]
+i = 1
+n = 3
+while i <= 100:
+	if mtest.is_prime(n):
+		n_primes.append(n)
+		i += 1
+	n += 2
 
 
 
@@ -56,7 +64,8 @@ class FactorizationMethod():
 		return (self.prime_1, self.prime_2)
 
 	def is_successful(self):
-		return self.prime_1 != None and self.prime_2 != None 
+		return self.prime_1 != None and self.prime_2 != None and self.prime_1 != 1 \
+			and self.prime_2 != 1 and self.prime_1 != self.mod and self.prime_2 != self.mod
 		# and self.prime_1 != 1 and self.prime_2 != 1
 
 	def is_on_mod(self):
@@ -64,68 +73,75 @@ class FactorizationMethod():
 
 class QuadraticSieveMethod(FactorizationMethod):
 
-	base_size = 30
+	base_size = 100
 	primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 33, 37]
-	randomizer = 0.1
+	randomizer = 1
 
 	def __init__(self):
 		FactorizationMethod.__init__(self)
 		self.max_iteration_lenght = SettingsSingleton.get_instance().get_iteration_count()
 		self.count = 0
-		
-		i = 0
-		test = MillerRabinTest()
-		self.primes = [2]
-		prime_timeout = test.get_timeout()
-		start = 3
-		with Pool(processes=1) as pool:
-			while len(self.primes) < self.base_size:
-				res = pool.apply_async(test.is_prime, args=(start,))
-				result = res.get(timeout=prime_timeout)
-				res.wait()
-				if result:
-					self.primes.append(start)
-				start += 2
+		if self.base_size < len(n_primes):
+			self.primes = n_primes[:self.base_size]
+		else:
+			self.primes = n_primes
+
+		self.base_size = len(self.primes)
+		# i = 
+		# test = MillerRabinTest()
+		# self.primes = [2]
+		# prime_timeout = test.get_timeout()
+		# start = 3
+		# with Pool(processes=1) as pool:
+		# 	while len(self.primes) < self.base_size:
+		# 		res = pool.apply_async(test.is_prime, args=(start,))
+		# 		result = res.get(timeout=prime_timeout)
+		# 		res.wait()
+		# 		if result:
+		# 			self.primes.append(start)
+		# 		start += 2
 
 	def attack(self, client):
 		FactorizationMethod.attack(self, client)
-		self.factorize()
-		self.count = 0
+		for i in range(self.max_iteration_lenght):
+			print("Trying for the "+str(i)+" time")
+			self.factorize()
+			if self.is_successful():
+				return
+			self.randomizer *= 1.2
 
 	def factorize(self):
+		bad_squares = 0
 		k = randint(1,self.mod)
-		self.count += 1
-		if self.count > self.max_iteration_lenght:
-			return
 		self.n_part = sqrt(self.mod)
 		self.squares = []
+		self.squares_set = set()
 		self.factor_base = []
-		while True:
-			for j in range(1,9):
-				i = randint(1, 9)*(1+self.randomizer)
-				b = int(sqrt(i)*self.n_part+j)
-				quad = b**b % self.mod
-				fact = []
-				for p in self.primes:
-					fact.append(0)
-					while quad % p == 0:
-						fact[-1] += 1
-						quad = quad / p
+		i = 1
+		with localcontext() as ctx:
+			ctx.prec = 10
+			while len(self.factor_base) <= 2*self.base_size:
+				i = randint(1, 10)*(self.randomizer)
+				for j in range(20):
+					b = int(i*self.n_part+j)
+					quad = pow(b,2,self.mod)
+					fact = []
+					for p in self.primes:
+						fact.append(0)
+						while quad % p == 0:
+							fact[-1] += 1
+							quad = quad / p
 
-				if quad == 1:
-					self.squares.append(b)
-					self.factor_base.append(fact)
-					break
-				"""
-				Else our primes are too small to give a correct factorization.
-				Choose another number.
-				"""
-			if len(self.factor_base) > len(self.primes)+4:
-				break
-			j += 1
-
+					if quad == 1 and not b in self.squares_set:
+						self.squares_set.add(b)
+						self.squares.append(b)
+						self.factor_base.append(fact)
+					"""
+					Else our primes are too small to give a correct factorization.
+					Choose another number.
+					"""
+				self.randomizer += 0.1
 		self.find_squares()
-
 
 	def find_squares(self):
 		matrix = np.asarray(self.factor_base).__mod__(2)
@@ -142,8 +158,6 @@ class QuadraticSieveMethod(FactorizationMethod):
 		for relation in relations:
 			if self.is_valid_relation(relation):
 				return
-		self.randomizer +=1
-		self.factorize()
 
 
 	def is_valid_relation(self, rows):
@@ -287,32 +301,47 @@ class LowExponentAttack(FactorizationMethod):
 		'''
 		C è candidato ad essere theta di eulero
 		'''
-		prec = 20
+		prec = 40
+		rounding = 0.1
 		with localcontext() as ctx:
 			ctx.prec = prec
-			print(self.key / self.mod, "Num")
 			e = Decimal(self.key)
 			n = Decimal(self.mod)
-			print(e/n, "Number")
-			rounding = 1 / prec
 			x, a, p, q = continued_fraction_next_step(self.key/self.mod,ctx,first=True)
 			i = 0
 			while i < prec+1:
 				x, a, p, q = continued_fraction_next_step(x,a,p,q,ctx)
 				if p[0] != 0:
 					C = (e * q[0] - 1) / p[0]
-					if C % 1 < rounding:
+					if C - int(C) < rounding or int(C) + 1 - C < rounding:
 						self.solve(int(C), rounding)
 						if self.is_successful():
 							break
-				i += 1
+				i += 2
 
 	def solve(self, theta, rounding):
 		p = [1, -self.mod + theta - 1, self.mod]
 		primes = np.roots(p)
-		if abs(primes[0] - int(primes[0])) < rounding  and abs(primes[1] - int(primes[1])) < rounding:
-			self.prime_1 = int(primes[0])
-			self.prime_2 = int(primes[1])
+		try:
+			prime_1 = int(primes[0])
+			prime_2 = int(primes[1])
+			if prime_1 == primes[0] and prime_2 == primes[1]:
+				self.prime_1 = prime_1
+				self.prime_2 = prime_2
+				return
+
+			if abs(primes[0] - prime_1) < rounding:
+				if self.mod % prime_1 == 0:
+					self.prime_1 = prime_1
+					self.prime_2 = self.mod / prime_1
+					return
+
+			if  abs(primes[1] - prime_2) < rounding:
+				if self.mod % prime_2 == 0:
+					self.prime_1 = self.mod / prime_2
+					self.prime_2 = prime_2
+		except:
+			pass
 
 	def is_on_mod(self):
 		return False
